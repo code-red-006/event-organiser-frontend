c16eab477838fa13764fc29fbd9e060b98e3707f
mishalp <mishalclever@gmail.com>
2022-12-31 15:38:08 +0530

    delete event added

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -4,6 +4,9 @@ import { useFetch } from '../hooks/useFetch';
 import './event.css'
 import addIcon from '../images/add.png'
 import AddEventForm from './AddEventForm';
+import bin from '../images/bin.png'
+import axios from 'axios';
+import { adminBaseURL } from '../constants';
 
 
 function EventList({url, isAdmin}) {
@@ -23,14 +26,32 @@ function EventList({url, isAdmin}) {
     navigate(`/admin/programs/${e.target.dataset.id}`)
   }
 
+  const removeEvent = (e) => {
+    e.stopPropagation()
+    const id = events[e.target.dataset.index]._id
+    const wantRemove = window.confirm(`Are you sure You want to delete ${events[e.target.dataset.index].event_name}`)
+    if(wantRemove){
+      const url = `${adminBaseURL}/events/delete/${id}`
+      const token = localStorage.getItem('token')
+      try {
+        axios.get(url, { headers: {'Authorization': `Bearer ${token}`}})
+        window.location.reload()
+      } catch (error) {
+        console.log(error);
+        window.location.reload()
+      }
+    }
+  }
+
 
   return (
     <div className='event-list'>
       <h2>Events</h2>
       <div className="cards-div">
-        {events && events.map((event)=>{
+        {events && events.map((event, index)=>{
           return <div key={event._id} className="card" onClick={viewPrograms} data-id={event._id}>
             <h3>{event.event_name}</h3>
+            <div className="remove"><img onClick={removeEvent} data-index={index} src={bin} alt="" /></div>
             </div>
         })}
         { isAdmin && <div  className="add-div">
