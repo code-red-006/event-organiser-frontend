d8655314b56030b7d046d9aa39ba110492a9af4b
mishalp <mishalclever@gmail.com>
2022-12-31 12:53:39 +0530

    add event form integrated with api

--- src/components/AddEventForm.js
+++ src/components/AddEventForm.js
@@ -1,8 +1,52 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { adminBaseURL } from '../constants';
+import Spinner from './Spinner';
 
 function AddEventForm() {
+
+    const [data, setData] = useState({event_name: ''});
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(null);
+    const navigate = useNavigate()
+    const handleChange = (e) => {
+        setData({event_name: e.target.value});
+    }
+
+    const handleSubmit = async(e) => {
+        setLoading(true)
+        e.preventDefault();
+        setError('');
+        const url = `${adminBaseURL}/events`;
+        try {
+            const token = localStorage.getItem('token');
+            const res = await axios.post(url, data, { headers: {'Authorization': `Bearer ${token}`}})
+            console.log(res);
+            window.location.reload()
+
+        } catch (error) {
+            setError(error);
+            console.log(error);
+
+        } finally{
+            setLoading(false)
+        }
+    }
+
   return (
-    <div>AddEventForm</div>
+    <div className='add-event-form'>
+        <h2>Add event</h2>
+        {loading && <Spinner loading={loading} />}
+        <form onSubmit={handleSubmit}>
+            <div className="name-div">
+                <label htmlFor="event_name">Event Name</label>
+                <input onChange={handleChange} value={data.event_name} type="text" name='event_name' required />
+                <input type="submit" />
+            </div>
+        </form>
+        {error && <span className='error'>{error}</span>} 
+    </div>
   )
 }
 
