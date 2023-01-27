e650fa08de6e4c527834f2637fcab313dfc3535c
Hamdan Salih <hamdansalihpc@gmail.com>
2023-01-27 19:23:06 +0530

    User Register form redesign

---
+++ src/pages/users/home/UserEvents.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import './UserEvents.css'
+
+function UserEvents() {
+
+
+  return (
+    <div className='user-events'>
+        <h2>Events</h2>
+        <div className='user-event-cards'>
+            <div>hi</div>
+            <div>my </div>
+            <div>boy </div>
+            <div>nice</div>
+        </div>
+    </div>
+  )
+}
+
+export default UserEvents
