7e94facb524463adab863f1b7ab855667a0304b0
nishad-aboobaker <nishuanshad@gmail.com>
2023-03-04 19:51:37 +0530

    style changed

--- src/pages/admin/login/AdminLogin.js
+++ src/pages/admin/login/AdminLogin.js
@@ -49,20 +49,22 @@ function AdminLogin() {
     <div className='admin-login'>
       
       {loading && <Spinner loading={loading} />}
-      <h2 className="title">Log in</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="username-div">
-          <label htmlFor="username">username</label>
-          <input onChange={handleChange} value={data.username} type="text" name='username' required />
-        </div>
-        <div className="password-div">
-          <label htmlFor="password">password</label>
-          <input onChange={handleChange} value={data.password} minLength="7" type="password" name='password' required />
-        </div>
-        <input type="submit" value="submit" />
-      </form>
+      <div className='contents'>
+        <h2 className="title">WELCOME</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="username-div">
+            <label htmlFor="username">username</label>
+            <input onChange={handleChange} value={data.username} type="text" name='username' required />
+          </div>
+          <div className="password-div">
+            <label htmlFor="password">password</label>
+            <input onChange={handleChange} value={data.password} minLength="7" type="password" name='password' required />
+          </div>
+          <input type="submit" value="submit" />
+        </form>
+
+      </div>
         {error && <span className='error'>{error}</span>} 
-      <p>Event Organizer</p>
     </div>
   )
 }
